321bedbf12930933585bfe31545a7e55a3580487
q5bel <belkin7495@gmail.com>
2022-11-22 18:35:54 +0700

    stable 2

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
 import os
 import threading
+import socket
+"""
+Здесь мы проверяем все ip адреса в диапазоне в локальной подсети
+нужно сделать:
+проверка - майнер ли это, задать словарь логинов паролей, вывод и мониторинг хэшрейта
++ отдельный файл, где бесконечный цикл с проверкой
+вопросы::::
+как сделать так, чтобы записывались активные майнеры и при этом опрашивались данные адреса постоянно
+нужен ли постоянный опрос для ВСЕЙ сети?
 
+"""
 print('---------- Start checking IP in list -----------')
 
 
@@ -13,8 +23,13 @@ def check_ip(ip):
 
 
 ip_list = []
+network = socket.gethostbyname(socket.gethostname())
+network_split = network.split('.')
+ip_head = ''
+for num in network_split[:-1]:
+    ip_head += num + '.'
 for last_digit in range(256):
-    ip_list.append(f'192.168.1.{last_digit}')
+    ip_list.append(f'{ip_head}{last_digit}')
 
 for ip in ip_list:
     ptk = threading.Thread(target=check_ip, args=[ip])
