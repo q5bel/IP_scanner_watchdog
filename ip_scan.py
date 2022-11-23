56b2f7c1d17fbcb3e4df83db00da24145ca2661d
q5bel <belkin7495@gmail.com>
2022-11-23 21:48:36 +0700

    version 3

--- ip_scan.py
+++ ip_scan.py
@@ -1,6 +1,18 @@
 import os
+import socket
+import ipaddress
 
 up_ip = {}
+
+
+def get_ip_list():
+    network = socket.gethostbyname(socket.gethostname()).split('.')
+    del network[-1]
+    net = ''
+    for i in network:
+        net += i + '.'
+    ip_list = ipaddress.ip_network(f'{net}0/24')
+    return ip_list
 
 
 def get_host_name(ip):  # ищем имя хоста по ip-адресу
--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 import threading
-import socket
-import ipaddress
 from pprint import pprint
-from server import *
+from ip_scan import *
 
 
 print('---------- Start checking IP in list -----------')
-
 # все основные действия
-network = socket.gethostbyname(socket.gethostname()).split('.')
-del network[-1]
-net = ''
-for i in network:
-    net += i + '.'
-
-ip_list = ipaddress.ip_network(f'{net}0/24')
+ip_list = get_ip_list()
 
 for ip in ip_list:
     ptk = threading.Thread(target=check_ip, args=[str(ip)])
     ptk.start()
-
-
 ptk.join()
 
 pprint(up_ip)
